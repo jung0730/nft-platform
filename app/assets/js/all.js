b94fccce6549f5801366b4cb8bcf58f783540652
rebecca <rebecca.yu@senao.com.tw>
2022-08-29 23:09:54 +0800

    未完成瀑布流

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,4 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+// 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
+$('.row').imagesLoaded().progress( function() {
+  $('.row').masonry(); // 渲染整體畫面
 });
