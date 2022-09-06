60cd8c84f0aae904b8f7abf4795588db7fb77df5
Rebecca <rebecca.yu@senao.com.tw>
2022-09-06 09:03:34 +0800

    Update 2022-09-06T01:02:47.627Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,38 +1,39 @@
+"use strict";
+
 // init Masonry
 var $grid = $('.grid').masonry({
   // options
   itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-});
+  columnWidth: '.grid-sizer'
+}); // layout Masonry after each image loads
 
-// layout Masonry after each image loads
-$grid.imagesLoaded().progress( function() {
+$grid.imagesLoaded().progress(function () {
   $grid.masonry('layout');
-});
-
-// document.addEventListener('DOMContentLoaded',function(){
+}); // document.addEventListener('DOMContentLoaded',function(){
 //   var collection = document.getElementById("Collection");
 //   collection.style.display = "none";
 // });
 
 function switchTab(evt, name) {
   // Declare all variables
-  var i, tabcontent, tablinks;
+  var i, tabcontent, tablinks; // Get all elements with class="tabcontent" and hide them
 
-  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tab-content");
-  console.log(tabcontent)
+  console.log(tabcontent);
+
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
-  }
+  } // Get all elements with class="tablinks" and remove the class "tab-active"
 
-  // Get all elements with class="tablinks" and remove the class "tab-active"
+
   tablinks = document.getElementsByClassName("tab-links");
+
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" tab-active", "");
-  }
+  } // Show the current tab, and add an "tab-active" class to the button that opened the tab
 
-  // Show the current tab, and add an "tab-active" class to the button that opened the tab
+
   document.getElementById(name).style.display = "block";
   evt.currentTarget.className += " tab-active";
 }
+//# sourceMappingURL=all.js.map
